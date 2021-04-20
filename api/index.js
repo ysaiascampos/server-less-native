2a09e668591d50ffa84a8b55940f30af98a54663
ysaiascampos <ysaiascampos@gmail.com>
2021-04-19 21:41:18 -0400

    inicial

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.send('Hola mundo')
-}
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('*', (req, res) => {
+    res.send('Hola api')
+})
+module.exports = app
