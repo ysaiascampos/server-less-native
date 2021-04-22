4e0326e3ad9b76ef079cf7ecf62e42f6b8281cef
ysaiascampos <ysaiascampos@gmail.com>
2021-04-22 09:20:43 -0400

    fix

--- api/models/Orders.js
+++ api/models/Orders.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Orders = mongoose.model('Order', new Schema({
-    meal_id: { typo: Schema.Types.ObjectId, ref: 'Meal'},
+    meal_id: { type: Schema.Types.ObjectId, ref: 'Meal'},
     user_id: String,
 }))
 
